120e6957416a6c1fe2fc592ade820201652d04d4
hamish-hood <hamishhood1999@gmail.com>
2022-11-27 10:39:31 +1100

    made a clearer print statement in the individual bikeshare question

--- bikeshare.py
+++ bikeshare.py
@@ -173,7 +173,7 @@ def trip_duration_stats(df):
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
+    print('\nCalculating Individual bikeshare user Stats...\n')
     start_time = time.time()
 
     # TO DO: Display counts of user types
